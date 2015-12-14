0077876734b1ff6ab8b7e534b2daa84bafbf08e3
Charles Piehl <charlespiehl@gmail.com>
2015-12-14 12:30:50 -0700

    ruby 3

---
+++ ruby/euler3.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+number = 600851475143
+divisor = 2
+while number > 1 do
+	if number % divisor == 0 then
+		number /= divisor
+		divisor -= 1
+	end
+	divisor += 1
+end
+
+puts(divisor)
