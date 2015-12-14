956e499a9c4bd817ae7d837729e4c65cc587d8f4
Charles Piehl <charlespiehl@gmail.com>
2015-12-13 17:32:47 -0700

    java 2

--- java/Euler2.java
+++ java/Euler2.java
@@ -1,9 +1,15 @@
-public class Euler1 {
+public class Euler2 {
 	public static void main(String[] args) {
 		int sum = 0;
-		for (int i = 0; i < 1000; i++)
-			if (i % 3 == 0 || i % 5 == 0)
-				sum += i;
+		int n = 1;
+		int lastn = 0;
+		while (n < 4000000) {
+			int newn = n + lastn;
+			if (newn % 2 == 0)
+				sum += newn;
+			lastn = n;
+			n = newn;
+		}
 		System.out.println(sum);
 	}
 }
