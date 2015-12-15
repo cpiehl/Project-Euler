2901c02e4d809ca66135e57869a82c5acc46f2d9
Charles Piehl <charlespiehl@gmail.com>
2015-12-15 16:11:17 -0700

    ruby 6

--- ruby/euler6.rb
+++ ruby/euler6.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/ruby
-# Project Euler #5
-# 2520 is the smallest number that can be divided by each of the numbers
-#   from 1 to 10 without any remainder.
-# What is the smallest positive number that is evenly divisible by all
-#   of the numbers from 1 to 20?
-# Answer: 232792560
+# Project Euler #6
+# Find the difference between the sum of the squares of the first one
+#   hundred natural numbers and the square of the sum.
+# Answer: 25164150
 
 squareofsums, sumofsquares = 0, 0
 
