bb905aa77cf76a9d071abf8af32e54c44ab877b4
Charles Piehl <charlespiehl@gmail.com>
2015-12-15 13:42:25 -0700

    python 4 fix

--- python/euler4.py
+++ python/euler4.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 result = -1
-for x in range(999,100,-1):
-	for y in range(999,100,-1):
+for x in range(100,1000):
+	for y in range(100,1000):
 		number = x * y
 		if str(number) == str(number)[::-1] and number > result:
 			result = number
