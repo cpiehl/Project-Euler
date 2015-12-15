149986b6b63b2434d3a54e00117956994f62ec7d
Charles Piehl <charlespiehl@gmail.com>
2015-12-15 13:26:28 -0700

    c++ 5 fix

--- c++/euler5.cpp
+++ c++/euler5.cpp
@@ -1,3 +1,11 @@
+/* Project Euler #5
+ * 2520 is the smallest number that can be divided by each of the numbers
+ *   from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all
+ *   of the numbers from 1 to 20?
+ * Answer: 232792560
+*/
+
 #include <iostream>
 
 int main()
@@ -6,9 +14,9 @@ int main()
 	bool found = false;
 	while (!found)
 	{
-		result += 1260;
+		result += 2520; // increment by value divisible by 1-10
 		found = true;
-		for (int i = 3; i <= 19; i++)
+		for (int i = 11; i <= 20; i++) // check divisible by 11-20
 		{
 			if (result % i != 0)
 			{
