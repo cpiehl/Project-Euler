8159acce16f32a9930aeaf03def762a215e4aeff
Charles Piehl <charlespiehl@gmail.com>
2015-12-16 22:09:15 -0700

    c++ 9

--- c++/euler8.cpp
+++ c++/euler8.cpp
@@ -1,4 +1,4 @@
-/* Project Euler #7
+/* Project Euler #8
  * The four adjacent digits in the 1000-digit number that
  *   have the greatest product are 9 × 9 × 8 × 9 = 5832.
  * 73167176531330624919225119674426574742355349194934
---
+++ c++/euler9.cpp
@@ -0,0 +1,26 @@
+/* Project Euler #8
+ * A Pythagorean triplet is a set of three natural numbers,
+ *   a < b < c, for which,
+ *   a^2 + b^2 = c^2
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ * Answer: 31875000
+*/
+
+#include <iostream>
+
+int main()
+{
+	for (int a = 1; a <= 1000; a++)
+	{
+		for (int b = a + 1; b <= 1000 - a; b++)
+		{
+			int c = 1000 - (a + b);
+			if (a*a + b*b == c*c)
+				std::cout << (a * b * c) << std::endl;
+		}
+	}
+	return 0;
+}
+
