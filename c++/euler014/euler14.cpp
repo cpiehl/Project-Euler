b06bce5d49ac6e4f8a5a8306ab4c3a01bd3e5afc
Charles Piehl <charlespiehl@gmail.com>
2016-01-29 22:47:02 -0700

    euler14 collatz sequence

---
+++ c++/euler014/euler14.cpp
@@ -0,0 +1,52 @@
+/* Project Euler #14
+ * Collatz iterative sequence defined as:
+ *   n -> n/2 if n is even
+ *   n -> 3n + 1 if n is odd
+ *
+ * Which starting number, under one million, produces the longest chain?
+ * Answer:   	837799
+*/
+
+#include <iostream>
+#include <stdexcept>      // std::out_of_range
+#include <map>
+using namespace std;
+
+int main()
+{
+	const unsigned int MAX = 1000000;
+	map<unsigned int,unsigned int> collatz_lengths;
+	int length, maxlen = 0, n_maxlen = -1;
+	for (unsigned int n = 2; n < MAX; n++)
+	{
+		// Calculate Collatz series length
+		length = 1;
+		for (unsigned int x = n; x != 1; )
+		{
+			if (collatz_lengths[x] != 0) // Already found
+			{
+				length += collatz_lengths[x] - 1;
+				break;
+			}
+			else
+			{
+				if (x % 2 == 0)
+					x = x / 2;
+				else
+					x = (3 * x) + 1;
+				length++;
+			}
+		}
+
+		collatz_lengths[n] = length;
+
+		if (length > maxlen)
+		{
+			maxlen = length;
+			n_maxlen = n;
+		}
+	}
+	cout << "Max: " << n_maxlen << " -> " << maxlen << endl;
+	return 0;
+}
+
