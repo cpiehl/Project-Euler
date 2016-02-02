29c5778c9292938a97dbaea35894cd04eec95fbb
Charles Piehl <charlespiehl@gmail.com>
2016-02-01 21:01:54 -0700

    c++ 31 coin sums

---
+++ c++/euler031/euler31.cpp
@@ -0,0 +1,29 @@
+/* Project Euler #31
+ * How many different ways can £2 be made using any number of coins?
+ * 1p, 2p, 5p, 10p, 20p, 50p, 100p, 200p
+ * Answer:    73682
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count = 0;
+
+	int target  = 200;
+
+	for (int a = target; a >= 0; a -= 200)
+		for (int b = a; b >= 0; b -= 100)
+			for (int c = b; c >= 0; c -= 50)
+				for (int d = c; d >= 0; d -= 20)
+					for (int e = d; e >= 0; e -= 10)
+						for (int f = e; f >= 0; f -= 5)
+							for (int g = f; g >= 0; g -= 2)
+								count++;
+
+	cout << count << endl;
+
+	return 0;
+}
+
